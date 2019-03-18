0d39ba8b9878f378badeaef21d61812ba1162718
CalvinXCui <cuihonggang@boco.com.cn>
2019-03-18 09:24:38 +0800

    修改数据

--- src/views/charts/echarts.vue
+++ src/views/charts/echarts.vue
@@ -39,7 +39,7 @@
                   title: { text: 'Column Chart' },
                   tooltip: {},
                   xAxis: {
-                      data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                      data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子",'其它']
                   },
                   yAxis: {},
                   series: [{
